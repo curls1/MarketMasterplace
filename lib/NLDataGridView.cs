4333a960b2051b1c8347ff415550a6f9eed5b3c2
jimonade <jimonade@gmail.com>
2014-04-02 09:00:27 -0400

    nldatagridview template

---
+++ lib/NLDataGridView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MarketMasterplace.lib
+
+{
+    class NLDataGridView : System.Windows.Forms.DataGridView
+    {
+
+        public void configureDefault()
+        {
+
+            this.AutoGenerateColumns = false;
+            this.AllowUserToAddRows = false;
+            this.AllowUserToDeleteRows = false;
+            this.AllowUserToOrderColumns = false;
+            this.AllowUserToResizeColumns = true;
+            this.AllowUserToResizeRows = true;
+            this.AlternatingRowsDefaultCellStyle.BackColor = Color.WhiteSmoke;
+            this.MultiSelect = false;
+            this.RowHeadersVisible = false;
+
+            this.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithAutoHeaderText;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+
+        }
+
+        public void configAutoSort()
+        {
+            foreach (DataGridViewColumn c in this.Columns)
+            {
+                c.SortMode = DataGridViewColumnSortMode.Automatic;
+            }
+
+        }
+
+    }
+
+}
